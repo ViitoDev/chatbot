246d27edab1b2eeb5d857da3ac5bb6153063c9f0
ViitoDev <joaovictorpiresdev@gmail.com>
2025-04-16 16:16:42 -0300

    Fazendo o funil

---
+++ chatbot.js
@@ -0,0 +1,33 @@
+//Inicia
+client.initialize();
+
+//Delay das ações
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+//Nome da empresa
+const enterprise = //fazer no site/app uma aba pra falar o nome da empresa
+
+//Funil
+client.on("message", async msg => {
+    if(msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola)/i) && msg.from.endsWith("@c.us")) {
+        const chat = await msg.getChat();
+
+        await delay(3000); 
+        await chat.sendStateTyping();
+        await delay(3000);
+        const contact = await msg.Contact();
+        const name = contact.pushname;
+        await client.sendMessage(msg.from, "Olá! "+ name.split(" ")[0] + "Sou o assistente virtual do " + enterprise + ". Como posso ajudar? Por favor, digite uma das opções abaixo: \n\n1 - Quais são os serviços?\n2 - Quero fazer um orçamento \n3 Outras perguntas");
+        await delay (3000);
+        await chat.sendStateTyping();
+        await delay (3000);
+    }
+
+    if (msg.body !== null && msg.body === "1" && msg.from.endsWith("@c.us")) {
+        const chat = await msg.getChat();
+        await delay(3000);
+        await chat.sendStateTyping();
+        await delay(3000);
+        await client.sendMessage(msg.from, "")
+    }
+})
